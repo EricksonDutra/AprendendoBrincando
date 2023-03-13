85822a6d0409baa2749471a38730a1a6a31b523d
EricksonDutra <ericksond10@gmail.com>
2023-03-13 13:17:48 -0400

    refactor somethings

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,12 @@
-import 'package:espaco_infantil/controllers/alunos_controller.dart';
-import 'package:espaco_infantil/repositories/alunos_repository.dart';
-import 'package:espaco_infantil/widgets/aluno_widget.dart';
-import 'package:espaco_infantil/widgets/drawer_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../models/aluno.dart';
+import '../widgets/aluno_grid.dart';
+import '../widgets/drawer_widget.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var controller = AlunosController();
-
-  @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<AlunosRepository>(context);
-    final List<Aluno> aluno = provider.alunos;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Espaço Infantil"),
@@ -37,56 +24,10 @@ class _HomePageState extends State<HomePage> {
               height: MediaQuery.of(context).size.height * 0.5,
             ),
             const SizedBox(height: 10),
-            GridView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              padding: const EdgeInsets.all(10),
-              itemCount: aluno.length,
-              itemBuilder: (ctx, i) => AlunoWidget(aluno: aluno[i]),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 3 / 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10),
-            ),
+            const AlunoGrid(),
           ],
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-//       SafeArea(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//           mainAxisSize: MainAxisSize.min,
-//           children: [
-//             Image.asset(
-//               'images/val.jpg',
-//               width: MediaQuery.of(context).size.width,
-//               height: MediaQuery.of(context).size.height * 0.5,
-//             ),
-//             const SizedBox(height: 40),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {
-//           Navigator.push(
-//             context,
-//             MaterialPageRoute(
-//               builder: (_) => const AddAlunosPage(),
-//             ),
-//           );
-//         },
-//         backgroundColor: Colors.pinkAccent,
-//         child: const Icon(Icons.add_reaction),
-//       ),
-//       drawer: const DrawerWidget(),
-//     );
-//   }
-// }
---
+++ lib/widgets/aluno_grid.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/aluno.dart';
+import '../repositories/alunos_repository.dart';
+import 'aluno_widget.dart';
+
+class AlunoGrid extends StatelessWidget {
+  const AlunoGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<AlunosRepository>(context);
+    final List<Aluno> aluno = provider.alunos;
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      padding: const EdgeInsets.all(10),
+      itemCount: aluno.length,
+      itemBuilder: (ctx, i) => AlunoWidget(aluno: aluno[i]),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
+    );
+  }
+}
